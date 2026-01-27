0fdeb68b88b4f1c82f835f3df8d90d3a542a7bee
IDGS-801-23001495 <sralanflores199@gmail.com>
2026-01-26 18:55:14 -0600

    macros

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    titulo="Flask IDGS801"
+    lista=["Juan", "Mario", "Pedro", "Dinenno"]
+    return render_template("index.html", titulo=titulo, lista=lista)
+
+
+
+
+@app.route("/operasBas", methods=["GET","POST"])
+def opera():
+    n1=0
+    n2=0
+    res=0
+    if request.method=='POST':
+        n1=request.form.get('n1')
+        n2=request.form.get('n2')
+        res=float(n1)+float(n2)
+    return render_template("operasBas.html", n1=n1, n2=n2, res=res)
+
+
+@app.route("/resultado", methods=["GET","POST"])
+def resultado():
+    n1=request.form.get('n1')
+    n2=request.form.get('n2')
+    tem=float(n1)+float(n2)
+    return f"La suma es: {tem}"
+
+
+
+
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
 
 @app.route("/hola")
 def hola():
@@ -44,4 +80,4 @@ def operas():
         '''
 
 if __name__ == "__main__":
-    app.run(debug=True) ##(debug=True) es para no tener ue estar reiniciando el servidor y se pueda actualizar en tiempo real
+    app.run(debug=True) ##(debug=True) es para no tener que estar reiniciando el servidor y se pueda actualizar en tiempo real
